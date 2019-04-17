5a4a9d375a92ca572a98302cf787118ee4e3ca7e
twojatata <patrykkuchar@gmail.com>
2019-04-17 08:13:39 +0200

    first commit

---
+++ src/main/java/com/wkbp/frontendbattleship/FrontendBattleshipApplication.java
@@ -0,0 +1,13 @@
+package com.wkbp.frontendbattleship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontendBattleshipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrontendBattleshipApplication.class, args);
+	}
+
+}
