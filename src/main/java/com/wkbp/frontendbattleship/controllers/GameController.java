f1edbe02afc0e9370aef5318619b3a62686de9b5
WIktor <wiktor-rup@wp.pl>
2019-04-18 13:59:10 +0200

    fix on #61

--- src/main/java/com/wkbp/frontendbattleship/controllers/GameController.java
+++ src/main/java/com/wkbp/frontendbattleship/controllers/GameController.java
@@ -2,16 +2,14 @@ package com.wkbp.frontendbattleship.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * @author Bartosz Kupajski
  */
 @Controller
-@RequestMapping("/game")
 public class GameController {
 
-    @GetMapping
+    @GetMapping("/game")
     public String game(){
         return "game";
     }
