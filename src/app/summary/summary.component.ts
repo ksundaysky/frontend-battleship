02ecd1a5ccc3942238da333606dda487efcacdfe
Krzysiek <krzysztof.niedzielski96@gmail.com>
2019-05-22 16:16:42 +0200

    add summary class, and send getSummary request to backend

--- src/app/summary/summary.component.ts
+++ src/app/summary/summary.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { SummaryService } from './summary.service';
+import { Summary } from './summary';
+
 
 @Component({
   selector: 'app-summary',
@@ -10,10 +12,21 @@ import { SummaryService } from './summary.service';
 export class SummaryComponent implements OnInit {
 
   gameId:number;
+  summary:Summary;
   constructor(private activatedRoute: ActivatedRoute, private summaryService: SummaryService) { }
 
   ngOnInit() {
     this.gameId = parseInt(this.activatedRoute.snapshot.paramMap.get('id'));
+
+    this.summaryService.getSummary(this.gameId).subscribe(
+      data=>{
+        this.summary = JSON.parse(data);
+        console.log(this.summary);
+      },
+      error=>{
+        console.log('cos poszlo nie tak :(')
+      }
+    );
   }
 
 }
